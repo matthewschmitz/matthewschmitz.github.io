ece4557d9efb07a6fc398dada5cb142af59a6455
Matthew Schmitz <matthewjsch@gmail.com>
2019-07-23 11:16:16 -0400

    Removed loading animation.

--- resources/js/loading.js
+++ resources/js/loading.js
@@ -1,4 +1,4 @@
-$(window).load(function() {
-		// Animate loader off screen
-		$(".se-pre-con").fadeOut("slow");;
-});
+//$(window).load(function() {
+		//Animate loader off screen
+		//$(".se-pre-con").fadeOut("slow");;
+//});
